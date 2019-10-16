ad1fcc63f3a53ecd1045ef18b369a10d633909a1
VaD1ke-edu <vladislav.slesarenko@gmail.com>
2019-10-16 08:11:54 +0300

    Add 6th exercise

---
+++ 2_vue_cli/exercise6/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <header></header>
+        <hr>
+        <div class="row">
+            <server-list></server-list>
+            <server-details></server-details>
+        </div>
+        <hr>
+        <footer></footer>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Header.vue';
+    import Footer from './components/Footer.vue';
+    import ServerList from './components/ServerList.vue';
+    import ServerDetails from './components/ServerDetails.vue';
+
+    export default {
+      'components': {
+          'header': Header,
+          'footer': Footer,
+          'server-list': ServerList,
+          'server-details': ServerDetails
+      }
+    };
+</script>
+
+<style>
+
+</style>

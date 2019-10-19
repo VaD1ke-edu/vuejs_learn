590221988fa4b7180147c6d94a6c0e63976a7c74
VaD1ke-edu <vladislav.slesarenko@gmail.com>
2019-10-20 00:29:41 +0300

    Add wonderful quotes project

---
+++ 3_project_quotes/src/components/Quote/Grid.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="quote-grid">
+        <div
+            v-for="(quote, index) in quotes"
+            class="quote-grid__item"
+        >
+            <app-quote-item
+                    @click.native="deleteQuote(index)"
+                    :quote="quote"
+            ></app-quote-item>
+        </div>
+    </div>
+</template>
+
+<script>
+    import quoteItem from './Item.vue';
+    import { vueQuoteBus } from './bus';
+
+    export default {
+        props: {
+            quotes: {
+                type: Array,
+                required: true
+            }
+        },
+
+        components: {
+            appQuoteItem: quoteItem
+        },
+
+        methods: {
+            deleteQuote(index) {
+                vueQuoteBus.$emit('delete_quote', index);
+            }
+        }
+    }
+</script>
+
+<style>
+    .quote-grid {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
+    .quote-grid__item {
+        width: 31.5%;
+        margin-right: 1%;
+        margin-bottom: 2rem;
+    }
+</style>
---
+++ 3_project_quotes/src/components/common/Info.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="info-wrapper bordered">
+        <div class="info">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped>
+    .info-wrapper {
+        padding: 2rem;
+
+        background-color: #48dbfb;
+        color: #2450d7;
+        text-align: center;
+    }
+</style>

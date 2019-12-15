6685a485fd4d83ee2f5a066d2858fe058caeda5b
VaD1ke-edu <vladislav.slesarenko@gmail.com>
2019-12-15 17:28:13 +0300

    Process on stock project

---
+++ 8_project_stock/src/components/Stock.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card-deck mb-2 text-center">
+        <template v-for="item in items">
+            <app-stock-card :title="item.title" classPostfix="stock"></app-stock-card>
+        </template>
+    </div>
+</template>
+
+<script>
+    import StockCard from './stock/Card.vue';
+    import {mapActions, mapGetters} from 'vuex';
+
+    export default {
+        components: {
+            'app-stock-card': StockCard
+        },
+        computed: mapGetters('stock', ['items'])
+    }
+</script>
+
+
---
+++ 8_project_stock/src/store/modules/stock.js
@@ -0,0 +1,35 @@
+const state = {
+    items: [
+        {
+            title: 'qwe1',
+        },
+        {
+            title: 'qwe2',
+        }
+    ]
+};
+
+const getters = {
+    items: state => {
+        return state.items;
+    }
+};
+
+const mutations = {
+    addItem: item => {
+        state.items = [...state.items, item];
+    }
+};
+
+const actions = {
+    addItem: ({commit}, payload) => {
+        commit('addItem', payload);
+    }
+};
+
+export default {
+    state,
+    getters,
+    mutations,
+    actions
+};
